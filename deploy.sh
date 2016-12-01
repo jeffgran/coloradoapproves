fc455fb982220736a9713fd835d1a11e3f1e75fe
Jeff Gran <jeff.gran@gmail.com>
2016-11-30 19:30:22 -0700

    more proofreading/tweaking updates

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ scp index.html bluehost:/home1/jeffgran/public_html/coloradoapproves/
 scp style.css bluehost:/home1/jeffgran/public_html/coloradoapproves/
 scp *.png bluehost:/home1/jeffgran/public_html/coloradoapproves/
 scp *.ico bluehost:/home1/jeffgran/public_html/coloradoapproves/
+scp manifest.json bluehost:/home1/jeffgran/public_html/coloradoapproves/
