68c0a8e066e9432749d953de9ce6db44f7c7d562
Jeff Gran <jeff.gran@gmail.com>
2016-11-30 18:48:13 -0700

    icons

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 scp index.html bluehost:/home1/jeffgran/public_html/coloradoapproves/
 scp style.css bluehost:/home1/jeffgran/public_html/coloradoapproves/
+scp *.png bluehost:/home1/jeffgran/public_html/coloradoapproves/
+scp *.ico bluehost:/home1/jeffgran/public_html/coloradoapproves/
