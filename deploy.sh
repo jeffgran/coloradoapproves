cfe1b82a1bf4641530a2a71ef9dd4e5db83fa41e
Jeff Gran <jeff.gran@gmail.com>
2016-12-01 22:52:00 -0700

    fix image deploy and opengraph image

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 scp index.html bluehost:/home1/jeffgran/public_html/coloradoapproves/
 scp style.css bluehost:/home1/jeffgran/public_html/coloradoapproves/
-scp images/* bluehost:/home1/jeffgran/public_html/coloradoapproves/
+scp -r images bluehost:/home1/jeffgran/public_html/coloradoapproves/
 scp manifest.json bluehost:/home1/jeffgran/public_html/coloradoapproves/
